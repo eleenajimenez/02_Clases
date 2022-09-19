d53119aedcf06e4ea02c3170e1528780b5da114f
elena <elenagomezlimon@gmail.com>
2022-09-19 09:58:54 +0200

    Agregar archivos de proyecto.

---
+++ 02_Clases/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Boolean salir =  false;
+
+        while (!salir)
+        {
+            
+        }
+    }
+}
