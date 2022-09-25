5267f9c0a69c06c67b2507e917703b2579502c73
elena <elenagomezlimon@gmail.com>
2022-09-25 16:51:24 +0200

    Clase biblioteca terminada (Ejercicio libros)

--- 02_Clases/Biblioteca.cs
+++ 02_Clases/Biblioteca.cs
@@ -1,17 +1,66 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace _02_Clases
 {
-    internal class Biblioteca
+    class Biblioteca
     {
         Libro l;
         ArrayList libros = new ArrayList();
+        bool valido;
+        int opcion;
         String titulo, autor, estilo, editorial;
+
+        public void menu()
+        {
+            do
+            {
+                do
+                {
+                    Console.WriteLine("¿Qué quieres realizar?: \n" +
+
+                    "1. Insertar libro. \n" +
+                    "2. Ver listado de libros. \n" +
+                    "3. Modificar libro. \n" +
+                    "4. Eliminar libro. \n" +
+                    "5. volver al menú principal.");
+
+                    valido = int.TryParse(Console.ReadLine(), out opcion);
+                    if (opcion >= 6) valido = false;
+                    if (!valido) Console.WriteLine("Introduzca una opcion valida");
+                } while (!valido);
+                Console.WriteLine("\n");
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("Ha elegido añadir libro.");
+                        insertarLibros();
+                        break;
+                    case 2:
+                        Console.WriteLine("Ha elegido ver el listado de libros.");
+                        mostrarLibros();
+                        break;
+                    case 3:
+                        Console.WriteLine("Ha elegido modificar un libro.");
+                        modificarLibro();
+                        break;
+                    case 4:
+                        Console.WriteLine("Ha elegido eliminar un libro.");
+                        eliminarLibro();
+                        break;
+                    case 5:
+                        Console.WriteLine("Volviendo al menú principal!");
+                        break;
+                }
+            } while (opcion != 5);
+        }
+
+
 
         public string pedirTeclado()
         {
@@ -20,7 +69,7 @@ namespace _02_Clases
             return texto;
         }
 
-        public void insertarLibros()
+        public Libro insertarLibros()
         {
             Console.WriteLine("Introduce el titulo: ");
             titulo = pedirTeclado();
@@ -32,25 +81,52 @@ namespace _02_Clases
             editorial = pedirTeclado();
             l= new Libro(titulo,autor,estilo,editorial);
             libros.Add(l);
+            return l;
         }
 
         
 
         public void mostrarLibros()
         {
+            Console.WriteLine("Libros en la biblioteca: ");
             for (int i = 0; i < libros.Count; i++)
             {
-                Console.WriteLine(libros[i]);
+                Console.WriteLine(i + ": " + libros[i]);
             }
         }
 
         public void modificarLibro()
         {
+            Console.WriteLine("Elige el libro que quieres modificar: ");
+            mostrarLibros();
+            int numero;
+            do
+            {
+                valido= int.TryParse(Console.ReadLine(), out numero);
+                if (numero > libros.Count - 1) valido = false;
+                if (!valido) Console.WriteLine("");
+            }while (!valido);
+            Libro l1= insertarLibros();
+            libros[numero] = l1;
+            
 
         }
 
         public void eliminarLibro()
         {
+            mostrarLibros();
+            Console.WriteLine("Elige el libro que quieres eliminar:" );
+            int numero;
+            valido= int.TryParse(Console.ReadLine(), out numero);
+            for(int i = 0; i < libros.Count; i++)
+            {
+                if (i == numero)
+                {
+                    libros.RemoveAt(numero);
+                    Console.WriteLine("Libro eliminado con exito.");
+                }
+                
+            }
 
         }
     }
--- 02_Clases/ManejarFechas.cs
+++ 02_Clases/ManejarFechas.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace _02_Clases
 {
-    internal class ManejarFechas
+    class ManejarFechas
     {
     }
 }
