f759410e2d1336aaba6a9eea409cbf9f13d28fe1
elena <elenagomezlimon@gmail.com>
2022-09-25 18:00:48 +0200

    Creación clases alquiler puerto

---
+++ 02_Clases/AlquilerPuerto/Alquiler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Clases.AlquilerPuerto
+{
+    class Alquiler
+    {
+        String nombre_cli;
+        String dni_cli;
+        DateTime fecha_inic;
+        DateTime fecha_fin;
+        String pos_amarre;
+        Barco barco;
+
+        
+    }
+}
---
+++ 02_Clases/AlquilerPuerto/Yate.cs
@@ -0,0 +1,12 @@
+﻿tusing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Clases.AlquilerPuerto
+{
+    internal class Yate
+    {
+    }
+}
